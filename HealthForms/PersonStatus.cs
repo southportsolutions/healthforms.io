06d96cc1a75775f1c1b53e5546c28fc9cff88a68
Robert H. Anstett <rob@southportsolutions.com>
2020-03-06 12:16:37 -0600

    initial commit.

---
+++ HealthForms/PersonStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HealthForms
+{
+    public class PersonStatus : IPersonStatus
+    {
+        public int PersonId { get; set; }
+        public int EventId { get; set; }
+        public int ExternalId { get; set; }
+        public int StatusCode { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
