80ac9c99586df6de11b36d86d6cfe229afc37125
Robert H. Anstett <rob@southportsolutions.com>
2020-03-06 12:16:37 -0600

    initial commit.

---
+++ HealthForms/IFormStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HealthForms
+{
+    public interface IPersonStatus
+    {
+        [JsonProperty(propertyName: "personId")]
+        int PersonId { get; set; }
+        [JsonProperty(propertyName: "eventId")]
+        int EventId { get; set; }
+        [JsonProperty(propertyName: "externalId")]
+        int ExternalId { get; set; }
+        [JsonProperty(propertyName: "statusCode")]
+        int StatusCode { get; set; }
+        [JsonProperty(propertyName: "updatedOn")]
+        DateTime UpdatedOn { get; set; }
+    }
+}
