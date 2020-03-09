8d7cb9c58d20c12a6479a8a0b53f8e35d74df0e7
Robert H. Anstett <rob@southportsolutions.com>
2020-03-09 11:51:53 -0500

    added the external person classes.

---
+++ HealthForms/AddPerson/IExternalPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthForms.AddPerson
+{
+    public interface IExternalPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string EmailAddress { get; set; }
+        string ExternalId { get; set; }
+        string GroupId { get; set; }
+    }
+}
