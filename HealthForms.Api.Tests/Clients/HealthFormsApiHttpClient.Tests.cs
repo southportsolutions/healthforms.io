f1190f2494549e157947306444073a7e530a48b2
Robert Anstett <rob@southportsolutions.com>
2024-01-03 14:54:43 -0600

    working on tests

---
+++ HealthForms.Api.Tests/Clients/HealthFormsApiHttpClient.Tests.cs
@@ -0,0 +1,20 @@
+﻿using HealthForms.Api.Clients;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace HealthForms.Api.Tests.Clients;
+
+public class HealthFormsApiHttpClientTests : UnitTestBase
+{
+    [Fact]
+    public async Task GetToken()
+    {
+        var httpClient = new HttpClient();
+        var client = new HealthFormsApiHttpClient(httpClient, Microsoft.Extensions.Options.Options.Create(HealthFormsApiOptions), new NullLogger<HealthFormsApiHttpClient>());
+        var response = await client.GetAccessToken();
+        Assert.NotNull(response);
+        Assert.NotNull(response.AccessToken);
+        Assert.NotNull(response.TokenType);
+        Assert.NotNull(response.ExpiresIn);
+        Assert.True(response.ExpiresOn > DateTime.Now);
+    }
+}
