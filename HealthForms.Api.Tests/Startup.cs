164efb1b6eb428dbc9464e4dabae80e8e7cf641b
Robert Anstett <rob@southportsolutions.com>
2024-01-03 14:54:43 -0600

    working on tests

---
+++ HealthForms.Api.Tests/Startup.cs
@@ -0,0 +1,36 @@
+﻿using HealthForms.Api.Options;
+using HealthForms.Api.Tests.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace HealthForms.Api.Tests;
+
+public static class Startup
+{
+    public static HealthFormsApiOptions Options { get; private set; } = new HealthFormsApiTestOptions();
+
+    public static HealthFormsApiOptions GetOptions()
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+                    
+            .AddJsonFile(Path.Combine((new DirectoryInfo(Environment.CurrentDirectory).Parent?.Parent?.Parent)?.ToString() ?? string.Empty, "appsettings.json"), true)
+            .AddEnvironmentVariables();
+        var config = configurationBuilder.Build();
+        Options = new HealthFormsApiTestOptions();
+        var section = config.GetSection(HealthFormsApiOptions.Key);
+        section.Bind(Options);
+
+        if (string.IsNullOrWhiteSpace(Options.ClientId))
+        {
+            Options.ClientId = Environment.GetEnvironmentVariable("HFPCLIENTID") ?? throw new InvalidOperationException();
+            Options.ClientSecret = Environment.GetEnvironmentVariable("HFPCLIENTSECRET") ?? throw new InvalidOperationException();
+        }
+
+        if (string.IsNullOrEmpty(Options.ClientId))
+        {
+            throw new Exception("Unable to get the HealthForms Client Info.");
+        }
+
+        return Options;
+
+    }
+}
--- HealthForms.Api/Options/HealthFormsApiOptions.cs
+++ HealthForms.Api/Options/HealthFormsApiOptions.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace HealthForms.Api.Options
+﻿namespace HealthForms.Api.Options
 {
     public class HealthFormsApiOptions
     {
         public const string Key = "HeatlhForms";
-        public string HostAddress { get; set; } = "https://app.healtforms.io/";
-        public string HostAddressAuth => $"{HostAddress}account/";
-        public string HostAddressApi => $"{HostAddress}api/";
+        public string HostAddress { get; set; } = "https://app.healthforms.io/";
+        public virtual string HostAddressAuth => $"{HostAddress}account/";
+        public virtual string HostAddressApi => $"{HostAddress}api/";
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string Scopes { get; set; } = "openid profile offline";
+        public string Scopes { get; set; } = "public_api";
     }
 }
