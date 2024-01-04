46c2dcb28c2e320e09055f3c4ccd47ff3ddde5fd
Robert H. Anstett <rob@southportsolutions.com>
2024-01-03 23:37:19 -0600

    working on scopes

--- HealthForms.Api.Tests/UnitTestBase.cs
+++ HealthForms.Api.Tests/UnitTestBase.cs
@@ -1,9 +1,14 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using HealthForms.Api.Options;
 
 namespace HealthForms.Api.Tests
 {
-    public abstract class UnitTestBase
+    public abstract class UnitTestBase<T> where T : class
     {
+        protected IFixture Fixture { get; } = new Fixture().Customize(new AutoMoqCustomization());
+        protected T ClassUnderTest { get; set; }
+
         public HealthFormsApiOptions HealthFormsApiOptions { get; }
 
         protected UnitTestBase()
