61875526245c767eedcfe5f9eb2a10d57a7e2052
Robert Anstett <rob@southportsolutions.com>
2024-02-04 16:28:36 -0600

    webhhok

---
+++ src/HealthForms.Api.Core/Models/Webhooks/WebhookData.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthForms.Api.Core.Models.Webhooks;
+
+public class WebhookData<T> where T : class
+{
+    public DateTime Timestamp { get; set; }
+    public WebhookType Type { get; set; }
+    public string EventId { get; set; }
+    public T Data { get; set; }
+}
