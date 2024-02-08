1185e8f1aa6101ece6058ca74fd1e4508a6ca6dd
Robert Anstett <rob@southportsolutions.com>
2024-02-07 21:12:50 -0600

    webhook classes

---
+++ src/HealthForms.Api.Core/Models/Webhooks/WebhookSession.cs
@@ -0,0 +1,5 @@
+﻿using HealthForms.Api.Core.Models.Sessions;
+
+namespace HealthForms.Api.Core.Models.Webhooks;
+
+public class WebhookSession : WebhookData<SessionResponse>;
---
+++ src/HealthForms.Api.Core/Models/Webhooks/WebhookSessionMember.cs
@@ -0,0 +1,5 @@
+﻿using HealthForms.Api.Core.Models.SessionMember;
+
+namespace HealthForms.Api.Core.Models.Webhooks;
+
+public class WebhookSessionMember : WebhookData<SessionMemberResponse>;
