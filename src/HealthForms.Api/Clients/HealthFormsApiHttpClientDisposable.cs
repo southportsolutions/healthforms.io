cf3e7d93f849b6876d547c18bf095e6554e2dee1
Robert Anstett <rob@southportsolutions.com>
2024-05-01 11:06:15 -0500

    added defautl constructor

--- src/HealthForms.Api/Clients/HealthFormsApiHttpClientDisposable.cs
+++ src/HealthForms.Api/Clients/HealthFormsApiHttpClientDisposable.cs
@@ -5,8 +5,12 @@ namespace HealthForms.Api.Clients;
 
 public class HealthFormsApiHttpClientDisposable : HealthFormsApiHttpClient, IDisposable
 {
-    public HealthFormsApiHttpClientDisposable(HttpClient httpClient, HealthFormsApiOptions options, ILogger<HealthFormsApiHttpClient>? log = null) 
+    public HealthFormsApiHttpClientDisposable(HttpClient httpClient, HealthFormsApiOptions options, ILogger<HealthFormsApiHttpClient>? log) 
         : base(httpClient, Microsoft.Extensions.Options.Options.Create(options), log)
+    {
+    }
+    public HealthFormsApiHttpClientDisposable(HttpClient httpClient, HealthFormsApiOptions options) 
+        : base(httpClient, Microsoft.Extensions.Options.Options.Create(options))
     {
     }
 
