4c26d0ee00ca96ecb106464a23c4ad80f03d5897
Robert Anstett <rob@southportsolutions.com>
2024-05-27 21:04:52 -0500

    fixed unit test options.

--- src/HealthForms.Api.Tests/Options/HealthFormsApiOptions.cs
+++ src/HealthForms.Api.Tests/Options/HealthFormsApiOptions.cs
@@ -4,6 +4,19 @@ namespace HealthForms.Api.Tests.Options
 {
     public class HealthFormsApiTestOptions : HealthFormsApiOptions
     {
+        private string? _hostAddressAuth;
+        public override string HostAddressAuth
+        {
+            get => _hostAddressAuth ?? $"{HostAddress}dev/account/";
+            set => _hostAddressAuth = value;
+        }
+
+        private string? _hostAddressApi;
+        public override string HostAddressApi
+        {
+            get => _hostAddressApi ?? $"{HostAddress}dev/api/";
+            set => _hostAddressApi = value;
+        }
         public string TenantToken { get; set; }
         public string SessionId { get; set; }
         public string TenantId { get; set; }
