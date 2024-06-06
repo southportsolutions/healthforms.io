029b8a08119347f6d230f299ebf5b762664c23b3
Robert Anstett <rob@southportsolutions.com>
2024-06-05 23:10:38 -0500

    updated response

--- src/HealthForms.Api/Shared/HealthFormsApiResponse.cs
+++ src/HealthForms.Api/Shared/HealthFormsApiResponse.cs
@@ -8,9 +8,6 @@ namespace HealthForms.Api.Shared
     public class HealthFormsApiResponse<T> : HealthFormsApiResponse where T : class?
     {
         public T? Data { get; set; }
-        public string? ErrorMessage { get; set; }
-        public bool IsSuccess => StatusCode is >= 200 and < 300;
-        public int StatusCode { get; set; }
     }
     public class HealthFormsApiResponse
     {
@@ -18,5 +15,21 @@ namespace HealthForms.Api.Shared
         public string? ErrorMessage { get; set; }
         public bool IsSuccess => StatusCode is >= 200 and < 300;
         public int StatusCode { get; set; }
+
+        public int? ErrorCode
+        {
+            get
+            {
+                if (ErrorMessage == null || ErrorMessage.Length < 4) return null;
+
+                var errorCodeString = ErrorMessage.Substring(0, 4);
+                if (int.TryParse(errorCodeString, out var errorCode))
+                {
+                    return errorCode;
+                }
+
+                return null;
+            }
+        }
     }
 }
