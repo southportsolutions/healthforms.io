b8cacd3e6e31d7c25a67a468f71b9fdbeceb033a
Robert Anstett <rob@southportsolutions.com>
2025-12-11 17:00:14 -0600

    Add FormTypeSelectResponse and update session request models for nullable properties

---
+++ src/HealthForms.Api.Core/Models/FormType/FormTypeSelectResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthForms.Api.Core.Models.FormType;
+
+public class FormTypeSelectResponse
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Instructions { get; set; }
+    public int? ValidityMonths { get; set; }
+}
--- src/HealthForms.Api.Core/Models/Sessions/SessionCreateRequest.cs
+++ src/HealthForms.Api.Core/Models/Sessions/SessionCreateRequest.cs
@@ -1,11 +1,13 @@
+using HealthForms.Api.Core.Models.FormType;
+
 namespace HealthForms.Api.Core.Models.Sessions;
 
-public class SessionCreateRequest : SessionCreateRequestBase
+public class SessionCreateRequest : SessionCreateRequestBase<SessionFormTypeRequest>
 {
-    public override string Name { get; set; }
+    public override string? Name { get; set; }
     public override DateTime StartDate { get; set; }
     public override DateTime EndDate { get; set; }
-    public override string Description { get; set; }
+    public override string? Description { get; set; }
     public override DateTime? InvitationSendDate { get; set; }
-    public override List<SessionFormTypeRequest> FormTypeIds { get; set; } = new();
+    public override List<SessionFormTypeRequest> FormTypes { get; set; } = new();
 }
