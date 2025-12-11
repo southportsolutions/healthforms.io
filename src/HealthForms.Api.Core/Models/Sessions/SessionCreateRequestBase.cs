bbe677fd6608368373764866b586fbdedc8e886e
Robert Anstett <rob@southportsolutions.com>
2025-12-11 11:46:23 -0600

    added session creation classes (#36)

---
+++ src/HealthForms.Api.Core/Models/Sessions/SessionCreateRequestBase.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HealthForms.Api.Core.Models.Sessions;
+
+public abstract class SessionCreateRequestBase
+{
+    public abstract string Name { get; set; }
+    public abstract DateTime StartDate { get; set; }
+    public abstract DateTime EndDate { get; set; }
+    public abstract string Description { get; set; }
+    public abstract DateTime? InvitationSendDate { get; set; }
+    public abstract List<SessionFormTypeRequest> FormTypeIds { get; set; }
+}
